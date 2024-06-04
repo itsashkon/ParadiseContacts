08472f89daf1ff5065feda9c35f0721cad03aec9
itsashkon <lavaasher@gmail.com>
2024-06-04 01:26:21 -0400

    Update editContacts.php

--- LAMPAPI/editContacts.php
+++ LAMPAPI/editContacts.php
@@ -1,17 +1,23 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $inData = getRequestInfo();
-$contactId = $inData["contactId"];
+$contactId = $inData["id"];
+
 if (empty($contactId)) {
     returnWithError("Contact ID is required");
+    exit();
 }
 
 // Map request field names to database column names
 $fieldMappings = [
-    "newPhoneNumber" => "PhoneNumber",
-    "newFirst"      => "FirstName",
-    "newLast"       => "LastName",
-    "newEmail"      => "EmailAddress"
+    "firstName"      => "FirstName",
+    "lastName"       => "LastName",
+    "emailAddress"   => "EmailAddress",
+    "phoneNumber"    => "PhoneNumber"
 ];
 
 $updateFields = [];
@@ -28,22 +34,24 @@ foreach ($fieldMappings as $requestField => $dbColumn) {
 
 if (empty($updateFields)) {
     returnWithError("No fields to update");
+    exit();
 }
 
 $sql = "UPDATE Contacts SET " . implode(", ", $updateFields) . " WHERE ID = ?";
 $updateValues[] = $contactId; // Add the contactId to the values
 $paramTypes .= "i"; // Add 'i' for the integer contactId
 
-
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
+    exit();
 } else {
     $stmt = $conn->prepare($sql);
     
     if (!$stmt) {
         returnWithError($conn->error);
+        exit();
     }
 
     // Bind parameters dynamically
@@ -58,7 +66,6 @@ if ($conn->connect_error) {
     $stmt->close();
     $conn->close();
 }
-
 
 function getRequestInfo()
 {
