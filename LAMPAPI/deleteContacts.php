06e79f742d064e95440375e1fedf63a0cc9e13df
itsashkon <lavaasher@gmail.com>
2024-06-04 01:39:46 -0400

    Update deleteContacts.php

--- LAMPAPI/deleteContacts.php
+++ LAMPAPI/deleteContacts.php
@@ -1,48 +1,55 @@
 <?php
-	$inData = getRequestInfo();
-	$phoneNumber = $inData["phoneNumber"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if($conn->connect_error)
-	{
-		returnWithError($conn->connect_error);
-	}
-	else{
-		$stmt = $conn->prepare("DELETE FROM Contacts WHERE PhoneNumber = ?");
-		$stmt->bind_param("s", $phoneNumber);
-		if($stmt->execute())
-		{
-			returnWithSuccess();
-		}
-		else
-		{
-			returnWithError($stmt->error);
-		}
-		$result = $stmt->get_result();
-		$stmt->close();
-		$conn->close();
-	}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$inData = getRequestInfo();
+$contactId = $inData["id"];
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithSuccess()
-	{
-    	$retValue = json_encode(array("error" => ""));
-    	sendResultInfoAsJson($retValue);
-	}
+if (empty($contactId)) {
+    returnWithError("Contact ID is required");
+    exit();
+}
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+    exit();
+} else {
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ?");
+    $stmt->bind_param("i", $contactId);
+    if ($stmt->execute()) {
+        returnWithSuccess("Contact deleted successfully");
+    } else {
+        returnWithError($stmt->error);
+    }
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithSuccess($message)
+{
+    $retValue = json_encode(array("error" => "", "message" => $message));
+    sendResultInfoAsJson($retValue);
+}
+
 ?>
